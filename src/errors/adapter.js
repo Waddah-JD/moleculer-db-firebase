d70b440ea18b9eb7fe6c9c73ffed7bdc00186894
waddah-jd <waddah.ja@gmail.com>
2021-10-16 17:55:01 +0300

    remove redundnant message overriding in custom errors

--- src/errors/adapter.js
+++ src/errors/adapter.js
@@ -1,18 +1,18 @@
 class MissingApiKeyError extends Error {
   constructor() {
-    super();
+    super(
+      "missing API key parameter: it should be passed as a first paramter to call: `new CloudFirestoreAdapter(apiKey, projectId)`"
+    );
     this.name = "MissingApiKeyError";
-    this.message =
-      "missing API key parameter: it should be passed as a first paramter to call: `new CloudFirestoreAdapter(apiKey, projectId)`";
   }
 }
 
 class MissingProjectIdError extends Error {
   constructor() {
-    super();
+    super(
+      "missing API key parameter: it should be passed as a second paramter to call: `new CloudFirestoreAdapter(apiKey, projectId)`"
+    );
     this.name = "MissingProjectIdError";
-    this.message =
-      "missing API key parameter: it should be passed as a second paramter to call: `new CloudFirestoreAdapter(apiKey, projectId)`";
   }
 }
 
