ed3fb6589c759c3c79a8fcdb9f7d2c0fc7818ddc
waddah-jd <waddah.ja@gmail.com>
2021-10-17 18:34:19 +0300

    add basic skeleton for int tests

---
+++ src/dbServiceMixin.int.test.js
@@ -0,0 +1,91 @@
+describe("direct action calls", () => {
+  it("should create a new document with a predefined ID", async () => {
+    // TODO implement me
+  });
+
+  it("should create a new document without explictly passing an ID", async () => {
+    // TODO implement me
+  });
+
+  it("should create another new document without an explict ID", async () => {
+    // TODO implement me
+  });
+
+  it("should get document by ID", async () => {
+    // TODO implement me
+  });
+
+  it("should find multiple documents by IDs", async () => {
+    // TODO implement me
+  });
+
+  it("should get all documents", async () => {
+    // TODO implement me
+  });
+
+  it("should find multiple documents by some condtion", async () => {
+    // TODO implement me
+  });
+
+  it("should find multiple documents by some condtion and return sorted results", async () => {
+    // TODO implement me
+  });
+
+  it("should find multiple documents by some condtion and return limited number of results", async () => {
+    // TODO implement me
+  });
+
+  it("should update a document", async () => {
+    // TODO implement me
+  });
+
+  it("should delete a document", async () => {
+    // TODO implement me
+  });
+});
+
+describe("action calls through API", () => {
+  it("should create a new document with a predefined ID", async () => {
+    // TODO implement me
+  });
+
+  it("should create a new document without explictly passing an ID", async () => {
+    // TODO implement me
+  });
+
+  it("should create another new document without an explict ID", async () => {
+    // TODO implement me
+  });
+
+  it("should get document by ID", async () => {
+    // TODO implement me
+  });
+
+  it("should find multiple documents by IDs", async () => {
+    // TODO implement me
+  });
+
+  it("should get all documents", async () => {
+    // TODO implement me
+  });
+
+  it("should find multiple documents by some condtion", async () => {
+    // TODO implement me
+  });
+
+  it("should find multiple documents by some condtion and return sorted results", async () => {
+    // TODO implement me
+  });
+
+  it("should find multiple documents by some condtion and return limited number of results", async () => {
+    // TODO implement me
+  });
+
+  it("should update a document", async () => {
+    // TODO implement me
+  });
+
+  it("should delete a document", async () => {
+    // TODO implement me
+  });
+});
